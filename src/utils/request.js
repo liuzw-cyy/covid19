3384a53fad3504cbb5c5101bff6fb18ad1d226ca
liuzw-cyy <3358795344@qq.com>
2022-05-25 12:15:43 +0800

    https

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,23 +3,23 @@ import axios from 'axios'
 const api = {
   // 疫情每日新增数据
   getNocv(params) {
-    return axios.get('http://api.tianapi.com//ncov/index', {
+    return axios.get('https://api.tianapi.com//ncov/index', {
       params
     })
   },
   // 中国所有省市疫情数据
   getNocvCity() {
-    return axios.get('http://iwenwiki.com/wapicovid19/all.php')
+    return axios.get('https://iwenwiki.com/wapicovid19/all.php')
   },
   // 单个省市的疫情数据
   getNocvSingleCity(params) {
-    return axios.get('http://iwenwiki.com/wapicovid19/guonei.php', {
+    return axios.get('https://iwenwiki.com/wapicovid19/guonei.php', {
       params
     })
   },
   // 世界各国疫情数据
   getNocvWorld(params) {
-    return axios.get('http://api.tianapi.com/ncovabroad/index', {
+    return axios.get('https://api.tianapi.com/ncovabroad/index', {
       params
     })
   },
